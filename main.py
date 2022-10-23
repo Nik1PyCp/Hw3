ef40c9c19dbb4062ba20b97764232442e5da3885
Veronika <vkotsubiak@gmail.com>
2022-10-23 20:01:48 +0300

    class Parrot

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ class Pets:
         self.hignrun = False
         self.fly = False
         self.highjump = False
+
 class Dog(Pets):
     def __init__(self):
         super().__init__()
         self.hignrun = True
         self.hignjump = False
         self.fly = False
+
+class Parrot(Pets):
+    def __init__(self):
+        super().__init__()
+        self.hignrun = False
+        self.hignjump = False
+        self.fly = True
